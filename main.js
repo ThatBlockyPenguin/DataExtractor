b3069a56ec69b409da0b399bf5ee564018844e4b
BlockyPenguin <me@blockypenguin.com>
2023-03-07 12:36:49 +0000

    Show data in new tab rather than console

--- main.js
+++ main.js
@@ -1,6 +1,49 @@
-console.log({
-  title: document.title,
-  language: document.documentElement.lang,
-  charset: document.characterSet,
-  doctype: document.doctype.name
-});
+(() => {
+  const container = document.createElement('div');
+  container.style.position = 'fixed';
+  container.style.top = '0';
+  container.style.right = '0';
+  container.style.zIndex = '9999';
+  container.style.margin = '1rem';
+  container.style.padding = '1rem';
+  container.style.backgroundColor = 'white';
+  container.style.borderRadius = '15px';
+  container.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.2)';
+  container.style.display = 'flex';
+  container.style.flexDirection = 'column';
+  container.style.alignContent = 'space-between';
+  container.style.justifyContent = 'space-between';
+  container.style.alignItems = 'center';
+  container.style.gap = '0.5rem';
+
+  const button = document.createElement('button');
+  button.style.backgroundColor = '#0d6efd';
+  button.style.color = 'white';
+  button.style.border = 'none';
+  button.style.borderRadius = '5px';
+  button.style.padding = '0.5rem 1rem';
+  button.style.margin = '0.5rem';
+  button.style.cursor = 'pointer';
+  button.style.boxShadow = '0 0 3px rgba(0, 0, 0, 0.2)';
+  
+  button.textContent = 'Open';
+  button.addEventListener('click', () => {
+    const tab = window.open('about:blank', '_blank');
+    tab.document.write(
+      '<pre style="font-size: 1.5rem; font-family: monospace;">' +
+        JSON.stringify({
+          title: document.title,
+          language: document.documentElement.lang,
+          charset: document.characterSet,
+          doctype: document.doctype.name
+        }, null, 2).replaceAll('"', '').replaceAll(',', '').slice(1, -1) +
+      '</pre>'
+    );
+    tab.document.close();
+  });
+
+  container.appendChild(document.createTextNode('View data:'));
+  container.appendChild(button);
+
+  document.body.appendChild(container);
+})();
